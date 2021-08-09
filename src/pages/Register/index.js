51d8e50b7316db95804b37df4698a35ef1b8d40b
estefaneluz <estefanecortes@gmail.com>
2021-08-09 11:44:05 -0300

    componente Register criado

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,52 @@
+import { TextField, Typography, Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import useStyles from "../../styles/form";
+import Password from "../../components/Password";
+
 export default function Register(){
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const styles = useStyles();
+
     return (
-        <h1>Cadastro</h1>
+        <div className={styles.container}>
+            <form className={styles.form} >
+                <Typography variant="h4" component="h2">
+                    Crie uma conta
+                </Typography>
+                <TextField 
+                    {...register("nome", {required: true})}
+                    error={!!errors.nome}
+                    label="Seu nome" 
+                />
+                <TextField 
+                    {...register("nome_loja", {required: true})}
+                    error={!!errors.nome_loja}
+                    label="Nome da loja" 
+                />
+                <TextField 
+                    {...register("email", {required: true})}
+                    error={!!errors.email}
+                    label="E-mail" 
+                    type="email"
+                />
+                <Password 
+                    register={() => register("senha", {required: true})}
+                    error={!!errors.senha}
+                    id="senha" label="Senha" 
+                />
+                <Password 
+                    register={() => register("repetir_senha", {required: true})}
+                    error={!!errors.repetir_senha}
+                    id="senha" label="Repita a senha" 
+                />
+                <div className={styles.action}>
+                    <Button type="submit" variant="contained" color="primary">
+                        Entrar
+                    </Button>
+                    <p>Já possui uma conta? <Link to="/">Acesse</Link></p>
+                </div>
+            </form>
+        </div>
     );
 }
