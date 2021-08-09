e98872533bb9796af451f4fc3d3ce8c352f93b14
estefaneluz <estefanecortes@gmail.com>
2021-08-09 00:41:47 -0300

    rota login criada

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 export default function Routes() {
   return (
-    <Login/>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+      </Switch>
+    </Router>
   );
 }
