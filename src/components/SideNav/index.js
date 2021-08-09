671e407425b21eeca6bd4ab5f6b2977d559b8fd5
estefaneluz <estefanecortes@gmail.com>
2021-08-09 20:13:02 -0300

    add useLocation para mudança de icone no sidenav

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -5,15 +5,21 @@ import store_img from "../../assets/store.svg"
 import close_img from "../../assets/close.svg"
 import { AuthContext } from "../../contexts/AuthContext"
 import { useContext } from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./styles.css"
 
 export default function SideNav(){
     const { setToken } = useContext(AuthContext);
+    const location = useLocation();
+
     return (
         <aside className="sidenav">
-            <Link to="/produtos"><img src={store_selected_img} /></Link>
-            <Link to="/perfil"><img src={user_img}/></Link>
+            <Link to="/produtos">
+                <img src={location.pathname ==="/produtos" ? store_selected_img : store_img} />
+            </Link>
+            <Link to="/perfil">
+                <img src={location.pathname ==="/perfil" ? user_selected_img : user_img}/>
+            </Link>
             <Link to="/" onClick={()=>setToken('')}><img src={close_img}/></Link>
         </aside>
     );
