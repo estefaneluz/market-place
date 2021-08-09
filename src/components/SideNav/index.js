79adca3d4a6af744f4c95a22c38ca5bcb1d875e9
estefaneluz <estefanecortes@gmail.com>
2021-08-09 20:00:50 -0300

    add Link do react-router-dom

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -3,14 +3,18 @@ import user_selected_img from "../../assets/user-selected.svg"
 import user_img from "../../assets/user.svg"
 import store_img from "../../assets/store.svg"
 import close_img from "../../assets/close.svg"
+import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react"
+import { Link } from "react-router-dom"
 import "./styles.css"
 
 export default function SideNav(){
+    const { setToken } = useContext(AuthContext);
     return (
         <aside className="sidenav">
-            <img src={store_selected_img} />
-            <img src={user_img}/>
-            <img src={close_img}/>
+            <Link to="/produtos"><img src={store_selected_img} /></Link>
+            <Link to="/perfil"><img src={user_img}/></Link>
+            <Link to="/" onClick={()=>setToken('')}><img src={close_img}/></Link>
         </aside>
     );
 }
