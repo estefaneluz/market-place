0877ea12236727f03c1005185e961f7cf8991970
estefaneluz <estefanecortes@gmail.com>
2021-08-10 16:49:52 -0300

    add estilo row

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,14 +7,16 @@ export default function Footer(){
     const styles = useStyles();
     const location = useLocation();
     return(
-    <footer>
+    <footer className="footer">
         <hr/>
-        <Link className={styles.link}>Cancelar</Link>
-        <Button 
-            className={styles.button}
-            variant="contained" 
-            color="primary">
-            {location.pathname ==="/perfil" ? "Editar perfil" : "Adicionar Produto"}
-        </Button>
+        <div className="row">
+            <Link className={styles.link}>Cancelar</Link>
+            <Button 
+                className={styles.button}
+                variant="contained" 
+                color="primary">
+                {location.pathname ==="/perfil" ? "Editar perfil" : "Adicionar Produto"}
+            </Button>
+        </div>
     </footer>);
 }
