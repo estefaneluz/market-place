75ca3a20a13285db914e309bf7e1a1cf0a0a2201
estefaneluz <estefanecortes@gmail.com>
2021-08-09 22:30:26 -0300

    funcao getUserData criada

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,23 +1,52 @@
 import { Typography, TextField } from "@material-ui/core";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 import useStyles  from "../../styles/form"; 
 
 export default function Profile(){
+    const [user, setUser] = useState({});
+    const { token, setLoading } = useContext(AuthContext);
     const styles = useStyles();
+
+    const getUserData = async () =>{
+        setLoading(true);
+
+        const request = await fetch('https://desafio-m03.herokuapp.com/perfil/', {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        setLoading(false);
+        
+        const response = await request.json();
+        return setUser(response);
+    }
+
+    useEffect(()=> {
+        getUserData();
+    }, [])
+
     return (
         <>
         <Typography variant="h4" component="h2"> Perfil </Typography>
+        { user.nome && 
         <form className={styles.column}>
             <TextField  disabled
                 label="Seu nome" 
+                defaultValue={user.nome}
             />
             <TextField disabled
-                label="Nome da loja" 
+                label="Nome da loja"
+                defaultValue={user.nome_loja} 
             />
             <TextField disabled
                 label="E-mail"
                 type="email" 
+                defaultValue={user.email}
             />
-        </form>
+        </form> }
         </>
     );
 }
