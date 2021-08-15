cef8ece585af2957234ce568e38293a9650e2a3d
estefaneluz <estefanecortes@gmail.com>
2021-08-15 02:11:45 -0300

    add setNameButton para consertar o nome do botão

--- src/components/ActionButtons/index.js
+++ src/components/ActionButtons/index.js
@@ -1,14 +1,24 @@
+import { useState, useEffect } from "react"
 import { Button } from "@material-ui/core"
 import { Link, useLocation, useHistory } from "react-router-dom"
 import useStyles from "../../styles/form"
 import "./styles.css"
 
 export default function ActionButtons({onSubmit}){
+    const [nameButton, setNameButton] = useState('');
     const styles = useStyles();
     const history = useHistory();
     const { pathname } = useLocation();
     const link = pathname.includes("produtos") ? "/produtos" : "/perfil"
     const submitButtonCondition = (pathname.includes("/novo") || pathname.includes("/editar")); 
+
+    useEffect(()=>{
+        setNameButton(()=>{
+            if(pathname.includes("editar")) return "Salvar Alterações"
+            else if(pathname.includes("produtos")) return "Adicionar Produto"
+            else if(pathname.includes("perfil")) return "Editar perfil"
+        })
+    }, [pathname])
 
     const defineRoute = (e) => {
         e.preventDefault();
@@ -32,7 +42,7 @@ export default function ActionButtons({onSubmit}){
                 className={styles.button}
                 variant="contained" 
                 color="primary">
-                {pathname.includes("editar") ? "Salvar Alterações" : "Adicionar Produto"}
+                {nameButton}
             </Button>
         </div>
     </div>);
